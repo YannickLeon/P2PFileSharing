fd05ca25d5c39a15cd082e5044076b444586d558
Yannick <yannick.maehner@gmx.de>
2025-01-21 15:57:37 +0100

    test sender and receiver for broadcasts

---
+++ sender.py
@@ -0,0 +1,21 @@
+import socket
+
+
+def send_broadcast(message, port=9000):
+    # Create a UDP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+    # Set the socket option to allow broadcasting
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    # Broadcast message to all devices on the local network (255.255.255.255)
+    # <broadcast> is a special address for broadcast
+    broadcast_address = ('<broadcast>', port)
+    sock.sendto(message.encode(), broadcast_address)
+    print(f"Sent broadcast message: {message}")
+
+    sock.close()
+
+
+# Send a broadcast message
+send_broadcast("Hello, this is a broadcast message!")
